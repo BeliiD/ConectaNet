9adff5a1f219998ccbe43b09eddff4bb0df0291b
Marsseloh <marcelosantanacorreia2008@gmail.com>
2025-06-17 17:39:27 -0300

    Atualização
    Barra de pesquisa agora está funcional;
    Recurso VLibras adicionado nas página.

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+//Barra de pesquisa
+
+const searchInput = document.getElementById('search-bar')
+
+searchInput.addEventListener('input', (event) => {
+    const value = formatString(event.target.value);
+
+    const itens = document.querySelectorAll("#modulos-list .modulos-item");
+    const lista = document.getElementById('modulos-list');
+
+    if(value != ''){
+        itens.forEach(item => {
+         if(formatString(item.textContent).indexOf(value) !== -1){
+              item.style.display = 'flex';
+              lista.style.display = 'flex';
+          } else{
+               item.style.display = 'none';
+           }
+        });
+    }else{
+        lista.style.display = 'none'
+    }
+});
+
+function formatString(value){
+    return value.toLowerCase().trim();
+}
